b555975a4f88638e8e8b95439b4dc306aff47a2c
Adharsh Kamath <adharshkamathr@gmail.com>
2020-04-16 23:00:20 +0530

    pt. someNumber

--- server/data/Mentors.js
+++ server/data/Mentors.js
@@ -22,6 +22,28 @@ const deleteMentor = function(mentorID) {
 	return dbQuery("CALL delete_mentor(?)", [mentorID]).then(() => true, (error) => new GraphQLError(error));
 };
 
+
+const addMentorToOrg = function (mentor_id, org_id) {
+	const startFunction = (org_id_length) => { if(org_id_length > 0) return dbQuery("CALL add_mentor_belongs_to(?,?)",[mentor_id, org_id[org_id_length-1]]).then(() => startFunction(org_id_length-1), (error) => new GraphQLError(error)); else if(org_id_length == 0) return mentor_id; };
+	return startFunction(org_id.length);
+};
+
+const removeMentorFromOrg = function (mentor_id, org_id) {
+	const startFunction = (org_id_length) => { if(org_id_length > 0) return dbQuery("CALL delete_mentor_belongs_to(?,?)",[mentor_id, org_id[org_id_length-1]]).then(() => startFunction(org_id_length-1), (error) => new GraphQLError(error)); else if(org_id_length == 0) return mentor_id; };
+	return startFunction(org_id.length);
+};
+
+const addMentorToProject = function (mentor_id, project_id) {
+	const startFunction = (project_id_length) => { if(project_id_length > 0) return dbQuery("CALL add_mentored_by(?,?)",[mentor_id, project_id[project_id_length-1]]).then(() => startFunction(project_id_length-1), (error) => new GraphQLError(error)); else if(project_id_length == 0) return mentor_id; };
+	return startFunction(project_id.length);
+};
+
+const removeMentorFromProject = function (mentor_id, project_id) {
+	const startFunction = (project_id_length) => { if(project_id_length > 0) return dbQuery("CALL delete_mentored_by(?,?)",[mentor_id, project_id[project_id_length-1]]).then(() => startFunction(project_id_length-1), (error) => new GraphQLError(error)); else if(project_id_length == 0) return mentor_id; };
+	return startFunction(project_id.length);
+};
+
+
 const MentorResolvers = {
 	id: (parent) => dbQuery("SELECT mentor_id FROM Mentors WHERE Mentors.mentor_id = (?)", [parent.mentor_id]).then((data) => data ? data.mentor_id : new GraphQLError("No such entry")),  
 	email: (parent) => dbQuery("SELECT email FROM Mentors WHERE Mentors.mentor_id = (?)", [parent.mentor_id]).then((data) => data ? data.email : new GraphQLError("No such entry")), 
@@ -31,4 +53,4 @@ const MentorResolvers = {
 	projects: (parent) => dbQuery("CALL get_projects_by_mentor(?)", [parent.mentor_id]).then((data) => data? data : new GraphQLError("No such entry")),
 };
 
-module.exports = {getMentors, addMentor, deleteMentor, MentorResolvers};
+module.exports = {getMentors, addMentor, deleteMentor, addMentorToOrg, removeMentorFromOrg, addMentorToProject, removeMentorFromProject, MentorResolvers};
