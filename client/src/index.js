8347aebaa512eab845e4f7688b21e382fbc5bd5b
Sriram Rao <sriramudupi19@gmail.com>
2020-04-18 23:05:38 +0530

    Set up apollo client with apollo react-hooks

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
