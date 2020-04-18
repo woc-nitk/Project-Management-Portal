1012e6ada795d4c9c8db99b8581684d1d845e7aa
Adharsh Kamath <adharshkamathr@gmail.com>
2020-04-18 17:39:30 +0530

    jwt stuff done

--- server/config/auth.js
+++ server/config/auth.js
@@ -3,33 +3,34 @@ const { GraphQLError } = require("graphql");
 const { dbQuery } = require("./db");
 const jwt = require("jsonwebtoken");
 const env = require("dotenv");
+const uuid = require("uuid");
 env.config();
 
-const hash = (password) => bcrypt.hash(password, 10, (error, hashedPassword) => { if(error) return new GraphQLError(error); return hashedPassword; });
-const compare = (password, passwordHash) => bcrypt.compare(password, passwordHash, (error, same) => { if(error) return new GraphQLError(error); return same; });
+const hash = (password) => bcrypt.hashSync(password, 10);
+const compare = (password, passwordHash) => bcrypt.compareSync(password, passwordHash);
 
-const genratejwt = (payload) => jwt.sign(payload,process.env.APP_SECRET);
+const generatejwt = (ID, type) => jwt.sign({ "id": ID, "type": type }, process.env.APP_SECRET, { expiresIn: "2hr" });
 
 const login = (email, password) => {
-	const year = new Date().getFullYear();
+    const year = new Date().getFullYear();
 	const checkMentor = (email, password) => { return dbQuery("SELECT mentor_id, mentor_password FROM Mentors WHERE email = (?) AND absolute_year = (?)",[email, year])
 		.then((data) => {   if(data && compare(password, data.mentor_password)) 
-			return { "id": data.mentor_id, "type": "mentor" }; 
-		else return new GraphQLError("Account not found"); }, 
+			{ return { "auth": generatejwt(data.mentor_id, "mentor"), "refresh": uuid.v4() }; }
+		else return new GraphQLError("Invalid Credentials"); }, 
 		(error) => new GraphQLError(error)); };
 	const checkOrgAdmin = (email, password) => { return dbQuery("SELECT org_admin_id, org_admin_password FROM Org_Admins WHERE email = (?) AND absolute_year = (?)",[email, year])
 		.then((data) => {   if(data && compare(password, data.org_admin_password)) 
-			return { "id": data.org_admin_id, "type": "orgAdmin" }; 
+			return { "auth": generatejwt(data.org_admin_id, "orgAdmin"), "refresh": uuid.v4() }; 
 		else return checkMentor(email, password); },
 		(error) => new GraphQLError(error)); };
 	const checkSuperAdmin = (email, password) => { return dbQuery("SELECT super_admin_id, super_admin_password FROM Super_Admins WHERE email = (?) AND absolute_year = (?)",[email, year])
 		.then((data) => {   if(data && compare(password, data.super_admin_password)) 
-			return { "id": data.super_admin_id, "type": "superAdmin" }; 
+			return { "auth": generatejwt(data.super_admin_id, "superAdmin"), "refresh": uuid.v4() }; 
 		else return checkOrgAdmin(email, password);  },
 		(error) => new GraphQLError(error)); };
 	const checkApplicant = (email, password) => { return dbQuery("SELECT applicant_id, applicant_password FROM Applicants WHERE email = (?) AND absolute_year = (?)",[email, year])
 		.then((data) => {   if(data && compare(password, data.applicant_password)) 
-			return { "id": data.applicant_id, "type": "applicant" }; 
+			return { "auth": generatejwt(data.applicant_id, "applicant"), "refresh": uuid.v4() }; 
 		else return checkSuperAdmin(email, password); }, 
 		(error) => new GraphQLError(error)); };
 	return checkApplicant(email, password);
