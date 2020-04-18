cab5439553b19bef9c8e2ac1a46272c9eb7cecc4
Sriram Rao <sriramudupi19@gmail.com>
2020-04-18 14:05:47 +0530

    Basic react frontend setup

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>NITK Winter of Code</h1>
+    </div>
+  );
+}
+
+export default App;
